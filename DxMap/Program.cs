db01ded82d5156b7efd54da756c7df17a9b4727e
Waescher, Andreas <andreas.waescher@chg-meridian.de>
2017-09-12 10:54:18 +0200

    Add project files.

---
+++ DxMap/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DxMap
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
